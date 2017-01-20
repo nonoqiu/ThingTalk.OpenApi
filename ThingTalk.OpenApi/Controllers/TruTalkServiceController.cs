befccd2399515d1a78cbf084e7f8514ad1145ad8
zaihui <121534082@qq.com>
2017-01-20 10:09:44 +0800

    20170120 Add RouteAttribute compatible WebService;

--- ThingTalk.OpenApi/Controllers/TruTalkServiceController.cs
+++ ThingTalk.OpenApi/Controllers/TruTalkServiceController.cs
@@ -1,21 +1,27 @@
-﻿using System.Web.Mvc;
-using ThingTalk.OpenApi.Application.Interfaces;
+﻿using System;
+using System.Web.Http;
 
 namespace ThingTalk.OpenApi.Controllers
 {
     /// <summary>
-    /// 
+    /// WebService请求控制器
     /// </summary>
-    public class TruTalkServiceController : Controller
+    public class TruTalkServiceController : ApiController
     {
         /// <summary>
-        /// 
+        /// 兼容之前的WebService请求
         /// </summary>
         /// <returns></returns>
-        //public string RequestPhoneData(string requestCmd, string requestData)
-        public string RequestPhoneData(string name, string id)
+        [HttpGet]
+        [Route("~/ThingTalkServices/TruTalkService.asmx/RequestPhoneData")]
+        public string RequestPhoneData(string requestCmd = "", string requestData = "")
         {
-            return "hello";
+            var requestUrl = this.Request.RequestUri.ToString().Replace("queryRequest", "requestData");
+            if (string.IsNullOrEmpty(requestData)) requestData = requestUrl.Substring(requestUrl.IndexOf("requestData") + "requestData".Length + 1);
+
+            Console.WriteLine("RequestUri: " + this.Request.RequestUri);
+
+            return "HELLO: " + requestCmd + ", " + requestData;
         }
     }
 }
